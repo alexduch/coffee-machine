8f01fae201ca8a8974870faa85eaaf344db2b546
Alexandre Duchenne <alexduch@hotmail.fr>
2021-02-22 19:47:23 +0100

    Iteration 1 - Define a simple interface for the coffee machine

--- src/main/java/com/github/alexduch/coffeemachine/CoffeeMachine.java
+++ src/main/java/com/github/alexduch/coffeemachine/CoffeeMachine.java
@@ -2,4 +2,7 @@ package com.github.alexduch.coffeemachine;
 
 public class CoffeeMachine {
 
+  public String translate(Order order) {
+    return "";
+  }
 }
--- src/test/java/com/github/alexduch/coffeemachine/CoffeeMachineTest.java
+++ src/test/java/com/github/alexduch/coffeemachine/CoffeeMachineTest.java
@@ -1,11 +1,16 @@
 package com.github.alexduch.coffeemachine;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 class CoffeeMachineTest {
 
+	private final CoffeeMachine coffeeMachine = new CoffeeMachine();
+
 	@Test
-	void contextLoads() {
+	void shouldTranslate() {
+		assertNotNull(coffeeMachine.translate(new Order()));
 	}
 
 }
